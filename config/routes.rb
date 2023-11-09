aeafd7a5fedbf2caa96cdb7295755e855a71ec97
Kathy Nguyen <kathy-nguyen@uiowa.edu>
2023-11-09 13:38:55 -0600

    edit forgot password link (customize)

--- config/routes.rb
+++ config/routes.rb
@@ -12,8 +12,11 @@ Rails.application.routes.draw do
   post 'login', to: 'sessions#create'
   get 'logout', to: 'sessions#destroy'
 
-  # route for forgot your password link
-  get '/forgot_password', to: 'sessions#forgot_password'
+  # routes for forgot your password link (password resets)
+  get '/password_resets/new', to: 'password_resets#new', as: 'new_password_reset'
+  post '/password_resets', to: 'password_resets#create'
+  get '/password_resets/edit/:id', to: 'password_resets#edit', as: 'edit_password_reset'
+  patch '/password_resets/update/:id', to: 'password_resets#update', as: 'update_password_reset'
 
   # route for the registration form
   get 'signup', to: 'registrations#new', as: :new_registration
