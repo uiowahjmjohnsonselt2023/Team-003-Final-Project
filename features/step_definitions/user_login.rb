6ce8c4fe462772a5dfcb3e089b63570b30e3541b
Kathy Nguyen <kathy-nguyen@uiowa.edu>
2023-11-09 01:14:16 -0600

    revise user login cucumber features and step definitions to align that when the user logs in succesfully, they will be directed to the home page of the marketplace

--- features/step_definitions/user_login.rb
+++ features/step_definitions/user_login.rb
@@ -2,24 +2,25 @@ Given('I am on the login page') do
   visit '/login'
 end
 
-When('I log in with valid credentials') do
-  fill_in 'Username', with: 'MaryAnn'
-  fill_in 'Password', with: 'password'
+When('I fill in the login form correctly') do
+  fill_in 'Username', with: 'valid_username'
+  fill_in 'Password', with: 'valid_password'
+end
+
+When('I click the login button') do
   click_button 'Login'
 end
 
-Then('I should be logged in to the system') do
-  puts page.body
-  expect(page).to have_content('Logged in successfully.')
+Then('I should be redirected to the home page') do
+  expect(current_path).to eq('/')
 end
 
-
-When('I log in with invalid credentials') do
-  fill_in 'Username', with: 'wrong_username'
-  fill_in 'Password', with: 'wrong_password'
-  click_button 'Login'
+When('I fill in the login form with invalid credentials') do
+  fill_in 'Username', with: 'invalid_username'
+  fill_in 'Password', with: 'invalid_password'
 end
 
 Then('I should see a login error message') do
   expect(page).to have_content('Username or password is invalid')
 end
+
--- features/user_login.feature
+++ features/user_login.feature
@@ -1,11 +1,13 @@
 Feature: User Login
 
-  Scenario: Successful login
+  Scenario: Successful login with valid credentials
     Given I am on the login page
-    When I log in with valid credentials
-    Then I should be logged in to the system
+    When I fill in the login form correctly
+    And I click the login button
+    Then I should be redirected to the home page
 
-  Scenario: Unsuccessful login
+  Scenario: Unsuccessful login with invalid credentials
     Given I am on the login page
-    When I log in with invalid credentials
+    When I fill in the login form with invalid credentials
+    And I click the login button
     Then I should see a login error message
