aed4cc48ca0fd93894600d8797680c8a06040484
Kathy Nguyen <kathy-nguyen@uiowa.edu>
2023-11-09 13:45:53 -0600

    add a password reset token, confirms whether the user's information (username and email) exists in the database before proceeding to reset the password

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -3,8 +3,19 @@ class PasswordResetsController < ApplicationController
   end
 
   def create
-  end
+    @user = User.find_by(username: params[:password_reset][:username], email: params[:password_reset][:email])
 
+    if @user
+      # generate a password reset token and reset the password
+      @user.update(reset_token: SecureRandom.urlsafe_base64)
+      @user.update(password: 'new_password')
+      flash[:notice] = "Password reset successfully. Your new password is 'new_password'."
+      redirect_to root_path
+    else
+      flash.now[:alert] = "Invalid username or email."
+      render :new
+    end
+  end
   def edit
   end
 
