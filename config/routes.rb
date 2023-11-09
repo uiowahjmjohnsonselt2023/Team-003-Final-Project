e8f9115ca9991a1809414f206a7fbf4b1e814172
Kathy Nguyen <kathy-nguyen@uiowa.edu>
2023-11-09 01:25:50 -0600

    successfully fixed route directions between logout <--> login page

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   # routes for session management (login and logout)
   get 'login', to: 'sessions#new', as: :login
   post 'login', to: 'sessions#create'
-  delete 'logout', to: 'sessions#destroy', as: :logout
+  get 'logout', to: 'sessions#destroy'
 
   # route for the registration form
   get 'signup', to: 'registrations#new', as: :new_registration
@@ -16,5 +16,4 @@ Rails.application.routes.draw do
 
   # health check route
   get "up" => "rails/health#show", as: :rails_health_check
-
 end
