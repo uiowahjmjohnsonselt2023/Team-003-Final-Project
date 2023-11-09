d2ef9a9fa82bb434525f16e60a8334d76088726b
Kathy Nguyen <kathy-nguyen@uiowa.edu>
2023-11-09 14:21:08 -0600

    remove duplicate migration (add user id to password resets)

--- app/models/password_reset.rb
+++ app/models/password_reset.rb
@@ -1,11 +1,3 @@
-class CreatePasswordResets < ActiveRecord::Migration[6.0]
-  def change
-    create_table :password_resets do |t|
-      t.references :user, foreign_key: true
-      t.string :token
-      t.datetime :created_at
-
-      t.timestamps
-    end
-  end
+class PasswordReset < ApplicationRecord
+  belongs_to :user
 end
