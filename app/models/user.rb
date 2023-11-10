256f7507cae0421c68d8476e6b8fdfa39384ff06
Kathy Nguyen <kathy-nguyen@uiowa.edu>
2023-11-09 18:37:00 -0600

    add associations for User, Product, Cart, and CartItem models

--- app/models/user.rb
+++ app/models/user.rb
@@ -21,5 +21,8 @@ class User < ApplicationRecord
   validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
   validates :password, presence: true, length: { minimum: 6 }
   has_one_attached :profile_picture
+
+  has_one :cart
+
 end
 
