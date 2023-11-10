926454588129efb078b3d7dd2881225a1803f5b6
Kathy Nguyen <kathy-nguyen@uiowa.edu>
2023-11-10 11:33:46 -0600

    fix products page styling to work

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -1,5 +1,5 @@
 class CartItemsController < ApplicationController
-  before_action :authenticate_user!
+  before_action :current_user
 
   def create
     cart = current_user.cart || current_user.create_cart
