c4cdd37568ef8f595ffe29a9bdc964ee27ddaf0d
Kathy Nguyen <kathy-nguyen@uiowa.edu>
2023-11-11 00:00:08 -0600

    working on a function so that if the user who is not logged in wants to make a purchase will be directed to the login page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,29 @@
 class ApplicationController < ActionController::Base
+  before_action :require_login
+
   helper_method :current_user
+  before_action :initialize_cart
 
   private
+
+  def require_login
+    unless user_logged_in?
+      flash[:alert] = 'You need to be logged in to make a purchase.'
+      redirect_to login_path
+    end
+  end
+
+  def initialize_cart
+    session[:cart] ||= Cart.new
+  end
+
+  def user_logged_in?
+    current_user.present?
+  end
+
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
-
   def authenticate_user!
     redirect_to login_url, alert: "You must be logged in to access this page." if current_user.nil?
   end
--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,7 +1,13 @@
 class OrdersController < ApplicationController
+  before_action :require_login, only: [:new, :create]
   def new
-    @cart_items = current_user.cart.cart_items
-    @order = Order.new
+    @cart = current_user.cart
+    if @cart.cart_items.empty?
+      @cart_empty = true
+    else
+      @cart_items = @cart.cart_items
+      @order = Order.new
+    end
   end
 
   def create
