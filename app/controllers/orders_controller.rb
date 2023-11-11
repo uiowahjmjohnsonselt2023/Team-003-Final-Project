c0eb09ea88576bc0ebfaf815fad474b93d50620a
Kathy Nguyen <kathy-nguyen@uiowa.edu>
2023-11-10 23:02:20 -0600

    implement new and create actions in OrdersController for purchase flow

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,13 +1,24 @@
 class OrdersController < ApplicationController
   def new
+    @order = Order.new
   end
 
   def create
+    @order = Order.new(order_params)
+    if @order.save
+      redirect_to @order, notice: 'Order was successfully placed.'
+    else
+      render :new
+    end
   end
 
   def show
+    @order = Order.find(params[:id])
   end
+  private
 
-  def index
+  def order_params
+    params.require(:order).permit(:user_id, :product_id, :quantity, :status)
   end
 end
+
