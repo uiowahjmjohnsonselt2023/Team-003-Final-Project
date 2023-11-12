d2b63dac656d22c60c0d8f6f90f5d0e9911c1dfd
Kathy Nguyen <kathy-nguyen@uiowa.edu>
2023-11-11 21:49:40 -0600

    Add total_price calculation method to Cart model

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -2,4 +2,9 @@ class Cart < ApplicationRecord
   belongs_to :user
   has_many :cart_items, dependent: :destroy
   has_many :products, through: :cart_items
+
+  # calculate total price of cart items
+  def total_price
+    cart_items.sum { |item| item.product.price * item.quantity }
+  end
 end
