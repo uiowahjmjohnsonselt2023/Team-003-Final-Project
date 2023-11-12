a4a076825f1f00886e301b3c119f1fb9098af9c0
Kathy Nguyen <kathy-nguyen@uiowa.edu>
2023-11-11 20:35:18 -0600

    fix OrderController and ApplicationController so that if the user is not logged in and wants to purchase, they must log in and will be directed to log in page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,13 +6,29 @@ class ApplicationController < ActionController::Base
 
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  rescue ActiveRecord::RecordNotFound
+    session[:user_id] = nil
+    nil
   end
 
+  def require_login
+    unless logged_in?
+      flash[:alert] = 'You must be logged in to access this section'
+      redirect_to login_url
+    end
+  end
+  def logged_in?
+    current_user.present?
+  end
   def initialize_cart
-    session[:cart] ||= Cart.new
+    @cart = Cart.find(session[:cart_id])
+  rescue ActiveRecord::RecordNotFound
+    @cart = Cart.create
+    session[:cart_id] = @cart.id
   end
 
   def authenticate_user!
     redirect_to login_url, alert: 'You must be logged in to access this page.' if current_user.nil?
   end
 end
+
