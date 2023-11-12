fcd1f14a7d33e954e9f8a27ebe7b25da60de60c8
Kathy Nguyen <kathy-nguyen@uiowa.edu>
2023-11-11 22:52:23 -0600

    add email attribute to Order database (for order confirmation email, contact information)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_12_044526) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_12_045134) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -80,6 +80,10 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_12_044526) do
     t.string "street"
     t.string "city"
     t.string "postal_code"
+    t.string "credit_card_number"
+    t.string "expiration_date"
+    t.string "cvv"
+    t.string "email"
     t.index ["product_id"], name: "index_orders_on_product_id"
     t.index ["user_id"], name: "index_orders_on_user_id"
   end
