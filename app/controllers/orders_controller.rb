69f196c2bd65ae70f4769bb79dba60506f4451a7
Kathy Nguyen <kathy-nguyen@uiowa.edu>
2023-11-11 22:36:27 -0600

    Implement Orders#create action to handle new orders, with success and failure paths

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -11,8 +11,36 @@ class OrdersController < ApplicationController
   end
 
   def create
+    @order = Order.new(order_params)
+    @order.user = current_user
+    @order.status = 'pending'
+
+    if @order.save
+      # call method to handle payment like `process_payment`
+      if process_payment(@order)
+        # Empty the current cart since the order has been placed
+        current_cart.empty_cart!
+
+        # redirect to the order show page with a success message
+        flash[:success] = "Order successfully placed! You can leave feedback once the order is fulfilled."
+        redirect_to order_path(@order)
+      else
+        # handle payment failure case
+        flash.now[:error] = "There was a problem with the payment. Please try again."
+        render :new
+      end
+    else
+      flash.now[:error] = "There was a problem placing your order. Please check your information."
+      render :new
+    end
+  end
+
+  def order_params
+  params.require(:order).permit(:street, :city, :postal_code, :credit_card_number, :expiration_date, :cvv, :quantity, :email, :phone, :additional_instructions, :save_payment_info)
+  end
+
+  def process_payment(order)
+    true
   end
 end
-def order_params
-  params.require(:order).permit(:street, :city, :postal_code, :credit_card_number, :expiration_date, :cvv, :quantity, :email, :phone, :additional_instructions, :save_payment_info)
-end
+
