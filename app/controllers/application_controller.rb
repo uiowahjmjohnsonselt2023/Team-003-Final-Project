8060293e734f24cfcc3152f2a0099c1e18ed1074
Kathy Nguyen <kathy-nguyen@uiowa.edu>
2023-11-11 20:29:11 -0600

    fix issues between WelcomeController and ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,19 +1,9 @@
 class ApplicationController < ActionController::Base
-  before_action :require_login, except: [:new]
   before_action :initialize_cart
-
   helper_method :current_user
 
   private
-  def require_login
-    unless user_logged_in?
-      flash[:alert] = 'You need to be logged in to make a purchase.'
-      redirect_to login_path
-    end
-  end
-  def user_logged_in?
-    current_user.present?
-  end
+
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
@@ -21,8 +11,8 @@ class ApplicationController < ActionController::Base
   def initialize_cart
     session[:cart] ||= Cart.new
   end
-  def authenticate_user!
-    redirect_to login_url, alert: "You must be logged in to access this page." if current_user.nil?
-  end
 
+  def authenticate_user!
+    redirect_to login_url, alert: 'You must be logged in to access this page.' if current_user.nil?
+  end
 end
