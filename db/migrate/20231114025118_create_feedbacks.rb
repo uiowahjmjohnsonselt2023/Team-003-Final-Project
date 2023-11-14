d4fe37dae3e2a618589eeb6617c6aff41799ea18
Kathy Nguyen <kathy-nguyen@uiowa.edu>
2023-11-13 20:51:30 -0600

    Create Feedback model with associated order reference

---
+++ db/migrate/20231114025118_create_feedbacks.rb
@@ -0,0 +1,10 @@
+class CreateFeedbacks < ActiveRecord::Migration[7.1]
+  def change
+    create_table :feedbacks do |t|
+      t.text :content
+      t.references :order, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
