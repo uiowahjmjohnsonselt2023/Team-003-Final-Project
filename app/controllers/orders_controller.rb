f857dcea3654642ba303ff8415281053eceb8571
Kathy Nguyen <kathy-nguyen@uiowa.edu>
2023-11-13 19:23:42 -0600

    add OrderItem Model to handle items for purchasing

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,41 +1,54 @@
 class OrdersController < ApplicationController
   before_action :require_login, only: [:new, :create]
   before_action :set_cart_and_items, only: [:new, :create]
+
   def new
-    @cart_empty = @cart.cart_items.empty?
-    @order = Order.new unless @cart_empty
+    if @cart.cart_items.empty?
+      redirect_to root_path, alert: "Your cart is empty."
+    else
+      @order = Order.new
+    end
   end
 
   def create
+    Rails.logger.debug "Processing new order..."
     @order = Order.new(order_params)
     @order.user = current_user
     @order.status = 'pending'
 
     if @order.valid?
       if process_payment(@order)
-        @order.save
-
-        # save each cart item to the order
-        @cart.cart_items.each do |item|
-          @order.order_items.create(product: item.product, quantity: item.quantity)
+        ActiveRecord::Base.transaction do
+          @order.save!
+          @cart.cart_items.each do |cart_item|
+            @order.order_items.create!(product: cart_item.product, quantity: cart_item.quantity)
+          end
+          @cart.destroy! # This empties the cart by destroying it and its associated items.
         end
-        @cart.empty_cart!
         redirect_to order_path(@order), notice: "Order was successfully placed."
       else
+        Rails.logger.error "Payment processing failed."
         flash.now[:error] = "There was a problem with the payment. Please try again."
         render :new
       end
     else
-      flash.now[:error] = "Please complete all required fields."
+      flash.now[:error] = @order.errors.full_messages.to_sentence
       render :new
     end
   end
 
   private
 
+  def require_login
+    unless current_user
+      flash[:error] = "You must be logged in to access this section"
+      redirect_to new_user_session_path # or your login route
+    end
+  end
+
   def set_cart_and_items
-    @cart = current_user.cart || Cart.new           # ensure @cart is not nil
-    @cart_items = @cart.cart_items.presence || []   # ensure @cart_items is not nil
+    @cart = current_user.cart || Cart.new
+    @cart_items = @cart.cart_items
   end
 
   def order_params
@@ -45,6 +58,7 @@ class OrdersController < ApplicationController
       :save_payment_info
     )
   end
+
   def process_payment(order)
     true
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_12_050031) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_14_012259) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -68,6 +68,16 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_12_050031) do
     t.string "images"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "order_items", force: :cascade do |t|
+    t.integer "quantity"
+    t.bigint "order_id", null: false
+    t.bigint "product_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["order_id"], name: "index_order_items_on_order_id"
+    t.index ["product_id"], name: "index_order_items_on_product_id"
   end
 
   create_table "orders", force: :cascade do |t|
@@ -139,6 +149,8 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_12_050031) do
   add_foreign_key "cart_items", "carts"
   add_foreign_key "cart_items", "products"
   add_foreign_key "carts", "users"
+  add_foreign_key "order_items", "orders"
+  add_foreign_key "order_items", "products"
   add_foreign_key "orders", "products"
   add_foreign_key "orders", "users"
   add_foreign_key "products", "users"
