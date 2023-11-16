73019aeadbac9c21e46844f395dcd4dc8621a864
Kathy Nguyen <kathy-nguyen@uiowa.edu>
2023-11-15 22:57:56 -0600

    generate migration for adding category to products model

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,9 +1,46 @@
 class ProductsController < ApplicationController
 
+  # display a list of all products
+  def index
+    @products = Product.all
+  end
+
+  # show details of a single product and its reviews
+  def show
+    @product = Product.find(params[:id])
+    @reviews = @product.reviews
+    # initialize an empty review object for the form
+    @review = Review.new
+  end
+
+  # add a product to the user's favorites
+  def add_to_favorites
+    @product = Product.find(params[:id])
+    current_user.favorites.create(product: @product)
+    flash[:notice] = "Product added to favorites."
+    redirect_to product_path(@product)
+  end
+
+  # (placeholder for 'message_seller' action if needed)
+
+  # write a review for a product
+  def write_review
+    @product = Product.find(params[:id])
+    @review = @product.reviews.build(review_params)
+    @review.user = current_user
+
+    if @review.save
+      flash[:notice] = "Review successfully submitted!"
+      redirect_to product_path(@product)
+    else
+      flash[:alert] = "There was a problem submitting your review."
+      render :show
+    end
+  end
+
   # search for products based on the provided query and filters
   def search
     @products = Product.all
-
     apply_search_query(params[:query])
     apply_filters(params[:category], params[:condition], params[:price_range])
     apply_sorting(params[:sort_by])
@@ -23,123 +60,51 @@ class ProductsController < ApplicationController
   def apply_filters(category, condition, price_range)
     @products = @products.where(category: category) if category.present?
     @products = @products.where(condition: condition) if condition.present?
+    apply_price_range_filter(price_range) if price_range.present?
+  end
 
-    if price_range.present?
-      range = price_range.split('-').map { |p| p.delete('$').to_i }
-      @products = @products.where(price: range[0]..range[-1])
-    end
+  # apply price range filter
+  def apply_price_range_filter(price_range)
+    range = price_range.split('-').map { |p| p.delete('$').to_i }
+    @products = @products.where(price: range[0]..range[-1])
   end
 
   # sort products based on the selected criteria
   def apply_sorting(sort_by)
-    case sort_by
-    when 'rating'
-      @products = @products.joins(:reviews)
-                           .select('products.*, COALESCE(AVG(reviews.rating), 0) as average_rating')
-                           .group('products.id')
-                           .order('average_rating DESC')
-    end
+    @products = apply_sorting_logic(@products, sort_by) if sort_by.present?
   end
 
-  # determine the sorting criteria based on the provided sort_by parameter
-  def sort_by_params(sort_by)
+  # define strong parameters for the review
+  def review_params
+    params.require(:review).permit(:content, :rating)
+  end
+
+  # determine the sorting logic based on the sort_by parameter
+  def apply_sorting_logic(products, sort_by)
     case sort_by
-    when 'newest'
-      { created_at: :desc }
     when 'best_selling'
-      { sales_count: :desc }
-    when 'price_low_to_high'
-      { price: :asc }
-    when 'price_high_to_low'
-      { price: :desc }
+      products.best_selling
     when 'featured'
-      { featured: :desc, created_at: :desc }
-    when 'rating'
-      # Ensure you have an average_rating method or column in your Product model
-      'average_rating DESC'
+      products.featured
     else
-      { created_at: :desc } # default sorting by newest
+      products.order(sort_by_params(sort_by))
     end
   end
-end
-
-
-  # display a list of all products
-  def index
-    @products = Product.all
-  end
-
-  # show details of a single product and its reviews
-  def show
-    @product = Product.find(params[:id])
-    @reviews = @product.reviews
-    @review = Review.new
-  end
-
-  # some action without a clear comment
-  def some_action
-    @user = User.find_by(id: some_id)
-  end
-
-  # add a product to the user's favorites
-  def add_to_favorites
-    @product = Product.find(params[:id])
-    current_user.favorites.create(product: @product)
-    flash[:notice] = "Product added to favorites."
-    redirect_to product_path(@product)
-  end
-
-  def message_seller
-  end
-
-  # write a review for a product
-  def write_review
-    @product = Product.find(params[:id])
-    @review = @product.reviews.build(review_params)
-    @review.user = current_user
-
-    if @review.save
-      flash[:notice] = "Review successfully submitted!"
-      redirect_to product_path(@product)
-    else
-      flash[:alert] = "There was a problem submitting your review."
-      render :show
-    end
-  end
-
-  private
 
   # helper method to determine the sorting parameters
   def sort_by_params(sort_option)
     case sort_option
     when 'newest'
       'created_at DESC'
-    when 'best_selling'
     when 'price_asc'
       'price ASC'
     when 'price_desc'
       'price DESC'
-    when 'featured'
-      'featured DESC, created_at DESC'
     else
-      'created_at DESC'
+      'created_at DESC' # default sorting by newest
     end
-
-    def apply_sorting(products, sort_option)
-      case sort_option
-      when 'best_selling'
-        products.best_selling
-      when 'featured'
-        products.featured
-      else
-        products.order(sort_by_params(sort_option))
-      end
-    end
-
-  # define strong parameters for the review
-  def review_params
-    params.require(:review).permit(:content, :rating)
   end
-  end
+end
+
 
 
