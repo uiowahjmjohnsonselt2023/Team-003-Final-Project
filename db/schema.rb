0355a065a3c94e0ff21c22d73907bac015d2a851
Kathy Nguyen <kathy-nguyen@uiowa.edu>
2023-11-16 14:36:55 -0600

    add migration for adding verified to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_16_045727) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_16_203622) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -164,6 +164,10 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_16_045727) do
     t.text "bio"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "reset_token"
+    t.string "reset_digest"
+    t.datetime "reset_sent_at", precision: nil
+    t.boolean "verified"
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
