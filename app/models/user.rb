edc5ad70b3e09a1df0f47ec64343b8d70adb2a73
Kathy Nguyen <kathy-nguyen@uiowa.edu>
2023-11-16 12:42:32 -0600

    add some reset password functionality to user model and revise the reset_password controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -21,5 +21,24 @@ class User < ApplicationRecord
 
   has_many :favorites
   has_many :favorite_products, through: :favorites, source: :product
+
+  def create_reset_digest
+    self.reset_token = User.new_token
+    update_columns(reset_digest: User.digest(reset_token), reset_sent_at: Time.zone.now)
+  end
+
+  def send_password_reset_email
+    UserMailer.password_reset(self).deliver_now
+  end
+
+  def self.new_token
+    SecureRandom.urlsafe_base64
+  end
+
+  def self.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+             BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
+  end
 end
 
