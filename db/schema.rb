0d10633854cd6f5356c0f4232132e625f879cb6f
Kathy Nguyen <kathy-nguyen@uiowa.edu>
2023-11-16 13:08:53 -0600

    create migration for AddResetDigestToUsers

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_16_190518) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_16_190812) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -165,6 +165,7 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_16_190518) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "reset_token"
+    t.string "reset_digest"
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
