bf8894b2e0134f1e608b7c9318efd9f2c67b3142
tim-lillig <lilligtimothy@gmail.com>
2023-11-20 14:52:18 -0600

    basic functionality implemented, needs integration with current sign in

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -8,7 +8,7 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     if user.present?
       sign_out_all_scopes
       flash[:success] = t 'devise.omniauth_callbacks.success', kind: 'Google'
-      sing_in_and_redirect user, event: :authentication
+      sign_in_and_redirect user, event: :authentication
     else
       flash[:alert] =
         t 'devise.omniauth_callbacks.failure', kind: 'Google', reason: "#{auth.info.email} is not authorized."
--- app/models/user.rb
+++ app/models/user.rb
@@ -21,7 +21,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable,
          :omniauthable, omniauth_providers: [:google_oauth2]
 
-  has_secure_password
+  #has_secure_password
   has_many :given_reviews, foreign_key: 'reviewer_id', class_name: 'Review'
   has_many :received_reviews, foreign_key: 'reviewee_id', class_name: 'Review'
   validates :username, presence: true, uniqueness: { case_sensitive: false }
