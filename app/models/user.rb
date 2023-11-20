0c581d2bc392ca5476ca92a1116203456ed6fa2a
tim-lillig <lilligtimothy@gmail.com>
2023-11-20 13:57:02 -0600

    Initial Rails code for google auth

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,6 +15,12 @@
 # associations:
 # - profile_picture (ActiveStorage) = stores the user's profile picture.
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable,
+         :omniauthable, omniauth_providers: [:google_oauth2]
+
   has_secure_password
   has_many :given_reviews, foreign_key: 'reviewer_id', class_name: 'Review'
   has_many :received_reviews, foreign_key: 'reviewee_id', class_name: 'Review'
@@ -26,6 +32,19 @@ class User < ApplicationRecord
   has_one :cart
   has_many :products
   has_many :reviews, dependent: :destroy
+
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.email = auth.info.email.downcase
+      user.password = Devise.friendly_token[0, 20]
+      user.full_name = auth.info.name
+      user.avatar_url = auth.info.image
+    end
+
+  end
+
+
+
 
 end
 
