c6a0882470ea05fdf53886c7801cb9d50230945f
Kathy Nguyen <kathy-nguyen@uiowa.edu>
2023-11-24 14:23:32 -0600

    run notifications functionality smoothly with no errors

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,2 +1,22 @@
 class NotificationsController < ApplicationController
+  before_action :set_notification, only: [:show]
+
+  # GET /notifications/1
+  def show
+    @notification.update(read_at: Time.zone.now) if @notification.unread?
+    @conversation = @notification.notifiable.conversation if @notification.notifiable.respond_to?(:conversation)
+
+    # redirect to the conversation if it exists, or to a fallback path otherwise
+    if @conversation
+      redirect_to conversation_path(@conversation)
+    else
+      redirect_to conversations_path, alert: 'Conversation could not be found.'
+    end
+  end
+
+  private
+
+  def set_notification
+    @notification = Notification.find(params[:id])
+  end
 end
--- app/models/notification.rb
+++ app/models/notification.rb
@@ -3,10 +3,16 @@ class Notification < ApplicationRecord
   belongs_to :actor, class_name: 'User'
   belongs_to :notifiable, polymorphic: true
 
-  scope :unread, -> { where(read_at: nil) }
+  def redirect_path
+   conversation_path(notifiable.conversation)
+  end
 
   # Scope to get unread notifications
   scope :unread, -> { where(read_at: nil) }
+
+  def unread?
+    read_at.nil?
+  end
 
   def mark_as_read
     unless read_at
