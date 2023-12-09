be1b9e3e3a537d8192fe437d177e82406b29c3a3
William Mitchell <william-j-mitchell@uiowa.edu>
2023-12-09 15:55:24 -0600

    Mailer Email change

--- app/mailers/user_verification_mailer.rb
+++ app/mailers/user_verification_mailer.rb
@@ -1,6 +1,6 @@
 # user_verification_mailer.rb
 class UserVerificationMailer < ApplicationMailer
-    default from: 'verification@notebay.com'
+    default from: 'selt2023team003@gmail.com'
     def verification_email(user)
       @user = user
       @verification_url = verify_account_url(token: @user.verification_token)
