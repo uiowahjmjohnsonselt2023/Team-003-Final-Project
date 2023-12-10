06c4103d7c1e1c96df08e2a0235bf9bfaad352f5
William Mitchell <william-j-mitchell@uiowa.edu>
2023-12-10 00:58:07 -0600

    Asset compilation

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,7 +27,7 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.asset_host = "http://assets.example.com"
@@ -78,8 +78,8 @@ Rails.application.configure do
     address:              'smtp.example.com',
     port:                 587,
     domain:               'example.com',
-    user_name:            '<username>',
-    password:             '<password>',
+    user_name:            ENV['GMAIL_USERNAME'],
+    password:             ENV['GMAIL_APP_PASSWORD'],
     authentication:       'plain',
     enable_starttls_auto: true
   }
