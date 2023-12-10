d0d429ec54771bea6d141be5c9f7a801c2baeec8
William Mitchell <william-j-mitchell@uiowa.edu>
2023-12-10 00:10:41 -0600

    Removed verification migration

--- db/migrate/20231110221111_add_verification_to_users.rb
+++
@@ -1,6 +0,0 @@
-class AddVerificationToUsers < ActiveRecord::Migration[7.1]
-  def change
-    add_column :users, :verified, :boolean
-    add_column :users, :verification_token, :string
-  end
-end
