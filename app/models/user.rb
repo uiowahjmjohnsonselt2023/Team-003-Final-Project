300f711cbb3a8e47ad32670ac75b75e034ecd43f
William Mitchell <william-j-mitchell@uiowa.edu>
2023-12-09 23:30:07 -0600

    User relationships relation

--- app/models/user.rb
+++ app/models/user.rb
@@ -47,7 +47,12 @@ class User < ApplicationRecord
   has_many :favorites
   has_many :favorite_products, through: :favorites, source: :product
 
+  has_many :active_relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  has_many :following, through: :active_relationships, source: :followed
 
+  has_many :passive_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+  has_many :followers, through: :passive_relationships, source: :follower
+  
   #associations for auctions
   has_many :bids, dependent: :destroy
 
@@ -69,7 +74,17 @@ class User < ApplicationRecord
         .order('average_rating DESC')
         .limit(5)
   end
+  def follow(other_user)
+    following << other_user unless self == other_user
+  end
 
+  def unfollow(other_user)
+    following.delete(other_user)
+  end
+
+  def following?(other_user)
+    following.include?(other_user)
+  end
   # allows for secure password management within the model by adding methods to set and authenticate against a BCrypt password
   has_secure_password
 
